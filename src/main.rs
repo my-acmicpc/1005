d974048ad3982f6e679b7634186ad19d7d86f2e6
Juyeong Maing <mjy9088@naver.com>
2024-03-03 22:02:55 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use std::{
+    collections::{HashMap, HashSet},
+    io,
+};
+
+fn main() {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    let test_case_count = line.trim().parse::<usize>().unwrap();
+    for _ in 0..test_case_count {
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+        let mut iter = line.trim().split(' ').flat_map(|x| x.parse::<usize>());
+        iter.next();
+        let path_count = iter.next().unwrap();
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+        let building_time = line
+            .trim()
+            .split(' ')
+            .flat_map(|x| x.parse::<u32>())
+            .collect::<Vec<_>>();
+        let mut path: HashMap<usize, HashSet<usize>> = HashMap::new();
+        for _ in 0..path_count {
+            let mut line = String::new();
+            io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.trim().split(' ').flat_map(|x| x.parse::<usize>());
+            let precondition = iter.next().unwrap() - 1;
+            let target = iter.next().unwrap() - 1;
+            if let Some(set) = path.get_mut(&target) {
+                set.insert(precondition);
+            } else {
+                let mut set = HashSet::new();
+                set.insert(precondition);
+                path.insert(target, set);
+            }
+        }
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+        let target_index = line.trim().parse::<usize>().unwrap() - 1;
+
+        let mut building_offset: Vec<Option<u32>> = vec![None; building_time.len()];
+        loop {
+            if let Some(result) = building_offset[target_index] {
+                println!("{}", result);
+                break;
+            }
+            building_offset = building_offset
+                .iter()
+                .enumerate()
+                .map(|(index, &option)| {
+                    if option != None {
+                        return option;
+                    }
+                    if let Some(set) = path.get(&index) {
+                        if set.iter().all(|&index| building_offset[index] != None) {
+                            Some(
+                                set.iter()
+                                    .map(|&index| building_offset[index].unwrap())
+                                    .fold(0, |acc, curr| acc.max(curr))
+                                    + building_time[index],
+                            )
+                        } else {
+                            None
+                        }
+                    } else {
+                        Some(building_time[index])
+                    }
+                })
+                .collect();
+        }
+    }
+}
